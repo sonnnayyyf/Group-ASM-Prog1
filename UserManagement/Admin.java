8d5904b4d7104c5fd3eb72a8f79da043281b0daa
hoangdv <duongviethoang2042002@gmail.com>
2023-09-04 02:49:24 +0700

    update

---
+++ UserManagement/Admin.java
@@ -0,0 +1,8 @@
+package UserManagement;
+
+public class Admin extends User {
+    public Admin(String userId, String username, String password) {
+        super(userId, username, password, "Admin");
+    }
+
+}
