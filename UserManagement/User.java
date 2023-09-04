4e7967922c84941432dd7c07c5abd4afda53639c
hoangdv <duongviethoang2042002@gmail.com>
2023-09-04 23:41:33 +0700

    update

--- UserManagement/User.java
+++ UserManagement/User.java
@@ -1,14 +1,19 @@
 package UserManagement;
 
+public enum Role {
+    ADMIN,
+    PORT_MANAGER
+}
+
 public class User {
     private String userId;
     private String username;
     private String password;
-    private String role;
+    private Role role;
 
     public User() {}
 
-    public User(String userId, String username, String password, String role) {
+    public User(String userId, String username, String password, Role role) {
         this.userId = userId;
         this.username = username;
         this.password = password;
@@ -39,13 +44,43 @@ public class User {
         this.password = password;
     }
 
-    public String getRole() {
+    public Role getRole() {
         return role;
     }
 
-    public void setRole(String role) {
+    public void setRole(Role role) {
         this.role = role;
     }
+
+    public boolean login(String username, String password) {
+        // Normally, you would get these details from a database.
+        String storedUsername = "sampleUsername";
+        String storedPassword = "samplePassword"; // This should actually be a securely hashed password.
+        Role storedRole = Role.ADMIN;
+
+        if (this.username != null) {
+            System.out.println("Already logged in.");
+            return false;
+        }
+
+        if (username.equals(storedUsername) && password.equals(storedPassword)) {
+            this.userId = generateUserId(); // Assume generateUserId() is another method that generates a unique ID.
+            this.username = storedUsername;
+            this.password = storedPassword; // In a real-world scenario, you would never store the password in plain text.
+            this.role = storedRole;
+            System.out.println("Logged in successfully.");
+            return true;
+        } else {
+            System.out.println("Login failed.");
+            return false;
+        }
+    }
+
+    private String generateUserId() {
+        // Generate a unique user ID. In a real application, you would probably fetch this from a database.
+        return "USER" + System.currentTimeMillis();
+    }
+
 
     public void logout() {
         this.userId = null;
