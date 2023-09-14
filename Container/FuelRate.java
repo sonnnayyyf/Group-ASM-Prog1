99d215c5db7866ab13e8980e7fad71e881b7d826
sonnnayyyf <122763787+sonnnayyyf@users.noreply.github.com>
2023-09-14 23:44:50 +0700

    adding fuelrate

---
+++ Container/FuelRate.java
@@ -0,0 +1,22 @@
+package Container;
+
+public class FuelRate {
+
+    // Class attributes
+    private double rateByShip;
+    private double rateByTruck;
+
+    // Getters and Setters
+    public double getRateByShip() {
+        return rateByShip;
+    }
+    public void setRateByShip(double rateByShip) {
+        this.rateByShip = rateByShip;
+    }
+    public double getRateByTruck() {
+        return rateByTruck;
+    }
+    public void setRateByTruck(double rateByTruck) {
+        this.rateByTruck = rateByTruck;
+    }
+}
